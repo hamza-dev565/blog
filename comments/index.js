929f642ffb1473f87361b3e7f0686ecafce38f6c
hamza-dev565 <hamzanaseer2@gmail.com>
2024-04-24 22:53:36 +0500

    added deployments for rest of the services

--- comments/index.js
+++ comments/index.js
@@ -1,49 +1,70 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
-const bodyParser = require('body-parser');
-
 const cors = require("cors");
+const axios = require("axios");
 
-app.use(cors({ origin: "http://localhost:3000" }));
+const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
+const commentsByPostId = {};
 
-const comments = {};
-
-app.get("/health", (req, res) => {
-  res.status(200).send("ok");
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.get("/posts/:postId/comments", (req, res) => {
-  const { postId } = req.params;
-
-  if (!comments[postId] || comments[postId].length === 0) {
-    res.status(200).send([]); // Send an empty array if no comments or postId doesn't exist
-  } else {
-    const postComments = comments[postId];
-    res.status(200).send(postComments);
-  }
-});
-
-app.post("/posts/:postId/comments", (req, res) => {
-  const { postId } = req.params;
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
-  console.log('req.body', req.body);
-  const id = randomBytes(4).toString("hex");
 
+  const comments = commentsByPostId[req.params.id] || [];
 
-  if (!comments[postId]) {
-    comments[postId] = []; // Initialize the array if it doesn't exist
+  comments.push({ id: commentId, content, status: "pending" });
+
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://event-bus-srv:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: "pending",
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post("/events", async (req, res) => {
+  console.log("Event Received:", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://event-bus-srv:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
   }
-  const comment = {id,content}
-  comments[postId].push(comment); // Push the content into the array
 
-  console.log('comments', comments);
-
-  res.status(200).send(comments[postId]);
+  res.send({});
 });
 
 app.listen(4001, () => {
-  console.log("listening on http://localhost:4001");
+  console.log("Listening on 4001");
 });
